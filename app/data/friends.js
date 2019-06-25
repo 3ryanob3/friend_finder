2e5963b1f28d4ec971a41e8587be721286296c43
Iby <Eryanobe@me.com>
2019-06-25 00:12:25 -0700

    updated Readme

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,16 +16,16 @@ var friends = [{
     },
     {
         name: "Sweet Dee",
-        photo: "https://vignette2.wikia.nocookie.net/itsalwayssunny/images/f/f3/Square-Dee-Cheesy.jpg",
+        photo: "https://blogs.forbes.com/olliebarder/files/2019/03/one_punch_man_s2_new.jpg",
         scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
     },
     {
         name: "Dennis",
-        photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/1/13/Dennis_%281%29.jpg",
+        photo: "https://www.wallpapermaiden.com/image/2018/11/27/genos-one-punch-man-cyborg-27424.jpeg",
         scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
     },
     {
-        name: "Mac",
+        name: "TeeTee",
         photo: "https://vignette3.wikia.nocookie.net/itsalwayssunny/images/5/5f/Mac_-_Season_8.jpg",
         scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
     },
