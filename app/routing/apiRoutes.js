450d833bb2d4c36be89c4b8f424fe70b738709c4
Iby <eryanobe@me.com>
2019-06-25 12:32:23 -0700

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,7 +12,7 @@ module.exports = function (app) {
         // Receive user details (name, photo, scores)
         var user = req.body;
 
-        // parseInt for scores
+        
         for (var i = 0; i < user.scores.length; i++) {
             user.scores[i] = parseInt(user.scores[i]);
         }
@@ -21,8 +21,7 @@ module.exports = function (app) {
         var bestFriendIndex = 0;
         var minimumDifference = 40;
 
-        // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
-        //  whatever the difference is, add to the total difference
+        // in this for-loop, start off with a zero difference and compare the user a
         for (var i = 0; i < friends.length; i++) {
             var totalDifference = 0;
             for (var j = 0; j < friends[i].scores.length; j++) {
@@ -30,17 +29,16 @@ module.exports = function (app) {
                 totalDifference += difference;
             }
 
-            // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+            // if there is a new minimum, change the best friend 
             if (totalDifference < minimumDifference) {
                 bestFriendIndex = i;
                 minimumDifference = totalDifference;
             }
         }
 
-        // after finding match, add user to friend array
+        // after finding match, add user 
         friends.push(user);
 
-        // send back to browser the best friend match
         res.json(friends[bestFriendIndex]);
     });
 };
